5c9fd8fcedc95c1fe812adb5381dff0b3f3236c1
unknown <giorgiapridonidze08@gmail.com>
2025-11-19 14:33:36 +0400

    created and styled Experience.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import About from "./components/About";
+import Expericne from "./components/Experience";
 import Header from "./components/Header";
 import Profile from "./components/Profile";
 import Technologies from "./components/Technologies";
 
 const App = () => {
   return(
-    <div className="app-container container-fluid">
+    <div className="app-container ">
 
       <Header />
 
-      <div className="app-body container-fluid  row">
+      <div className="app-body row">
         
-        <div className="body-left col col-3">
+        <div className="body-left col col-12 col-md-4 mb-4 mb-md-0">
         
           <Profile />
         
         </div>
 
-        <div className="body-right col-9">
+        <div className="body-right col-12 col-md-8">
           <About />
           <Technologies />
+          <Expericne />
         </div>
 
       </div>
---
+++ frontend/src/components/Experience.jsx
@@ -0,0 +1,35 @@
+import upworkImg from '../assets/tech imgs/upwork.png'
+const Expericne = () => {
+    return(
+        <div className="experience-container p-3">
+            <div className="experience-header">
+                <h3>Work Experience</h3>
+            </div>
+            <div className="experience-body p-2">
+                <div className="experince">
+                    <div className="experience-header d-flex align-items-center gap-4">
+                        <div className="header-left">
+                            <img src={upworkImg} className='rounded-2' alt="" style={{maxWidth: '60px'}}/>
+                        </div>
+                        <div className="header-right">
+                            
+                            <h5>Freelancer Frontend Developer At Upwork</h5>    
+                            <h6 className='text-secondary'>2023 - July</h6>
+                        </div>
+                    </div>
+                    
+                    <div className="experience-body ps-4 border-start border-bottom w-75 w-sm-50 border-secondary ms-4">
+                        <ul>
+                            <li>Developer FrontEnd Side Of Login/Signup Page.</li>
+                            <li>Handled User's Input And Errors.</li>
+                            <li>Communicated With Client on Daily Working Basis.</li>
+                        </ul>
+                    </div>
+                    <div className="experience-footer"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Expericne;
