392626a4debdcd7c80c3bfb96f3b3742b4daa254
unknown <giorgiapridonidze08@gmail.com>
2025-11-19 13:44:52 +0400

    feat : created and styled Profile.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import Header from "./components/Header";
+import Profile from "./components/Profile";
 
 const App = () => {
   return(
-    <div className="app-container">
+    <div className="app-container container-fluid">
 
       <Header />
+
+      <div className="app-body ">
+        
+        <div className="body-left">
+        
+          <Profile />
+        
+        </div>
+
+        <div className="body-right"></div>
+      </div>
+
+
     </div>
   );
 };
---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import avatar from '../assets/avatar.png'
+
+const Profile = () => {
+    return(
+        <div className="profile-container w-25 mt-4">
+            <div className="profile-image">
+            
+                <img src={avatar} alt="Profile Avatar" className='w-100 h-100' style={{borderRadius : '100%'}}/>
+            
+            </div>
+
+            <div className="profile-body ps-4">
+                <div className="titles py-2">
+                    <div className='name mb-3'><span className='fs-2'>Giorgi</span> <br /> <span className='fs-4 text-secondary'>Apridonidze</span></div>
+                    <h5 >Fullstack Web Developer</h5>
+                </div>
+                <div className="location">
+                    <h5><i class="fa-solid fa-location-dot"></i> Georgia , Tbilisi</h5>
+                </div>
+            </div>
+
+            <div className="profile-contact d-flex flex-column gap-1 ps-4 py-2">
+
+                <a className='link-underline link-offset-3 link-underline-opacity-0 link-underline-opacity-100-hover' href="https://www.linkedin.com/in/giorgi-aphridonidze-7aa896291/"><i class="fa-brands fa-square-linkedin"></i>in/giorgi-aphridonidze</a>
+                <a className='link-underline link-offset-3 link-underline-opacity-0 link-underline-opacity-100-hover' href="https://github.com/Apridonidze"><i class="fa-brands fa-square-github"></i>Apridonidze</a>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
