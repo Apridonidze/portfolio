9cec9cc174af12948b792842e23c19dc343ebc0d
unknown <giorgiapridonidze08@gmail.com>
2025-11-20 15:09:06 +0400

    styled Alert.jsx component for responsive design

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,11 +1,11 @@
 const Alert = ( { setToggleAlert , toggleAlert } ) => {
     return(
-        <div className="alert-container position-fixed border start-50 text-center p-5 rounded-2 border-secondary" style={{backgroundColor : '#0d1117'}}>
-            <h3>{toggleAlert.text}</h3>
+        <div className="alert-container position-fixed border text-center p-5 rounded-2 border-secondary" style={{backgroundColor : '#0d1117'}}>
+            <h3 className="pb-3">{toggleAlert.text}</h3>
             <button className="btn btn-primary w-50" onClick={() => setToggleAlert({status:false , text: null})}>Ok</button>
         </div>
-    )
-}
+    );
+};
 
 
-export default Alert
+export default Alert;
