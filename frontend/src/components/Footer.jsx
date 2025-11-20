d7478caf44345ae67ebedbfbd1ab29734ca6cb90
unknown <giorgiapridonidze08@gmail.com>
2025-11-20 14:21:00 +0400

    styled Footer.jsx comp[onent for responsive design and addded content in it

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,19 @@
 const Footer = () => {
     return(
-        <div className="footer-container">
-            Footer
+        <div className="footer-container container-fluid text-secondary px-5 py-3">
+            <div className="row row-cols-1 row-cols-sm-3 g-3">
+                <div className="footer-left col">
+                <h6>© 2025 Portfolio</h6>
+                </div>
+                <div className="footer-center col">
+                    <a href="" className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">link</a>
+                    <a href="" className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">link</a>
+                    <a href="" className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">link</a>
+                </div>
+                <div className="footer-right col ">
+                    <h6>Created By Giorgi Aphridonidze</h6>
+                </div>
+            </div>
         </div>
     );
 };
