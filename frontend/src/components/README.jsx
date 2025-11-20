2cf774add1e7f7a61f1968809de679c9825b07b6
unknown <giorgiapridonidze08@gmail.com>
2025-11-20 20:53:00 +0400

    fixed overflow issues in project

--- frontend/src/components/README.jsx
+++ frontend/src/components/README.jsx
@@ -4,7 +4,7 @@ import Technologies from "./Technologies"; //importing component
 
 const README = () => {
     return(
-        <div className="readme-container container border bored-1 border-secondary mt-3 ms-3 rounded-1 p-2 " >
+        <div className="readme-container container border bored-1 border-secondary mt-3 mx-1 rounded-1 p-2 " >
             <div className="readme-header border-bottom border-1 border-secondary py-2">
                 <h6 className="px-2 text-secondary">Giorgi Apridonidze / README.md</h6>
             </div>
