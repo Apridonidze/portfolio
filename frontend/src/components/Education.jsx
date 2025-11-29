c826f2c4caa606a5a23041afa3545138cc2a6a03
unknown <giorgiapridonidze08@gmail.com>
2025-11-30 02:40:19 +0400

    feat : modified Education.jsx

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -6,9 +6,8 @@ const Education = () => {
                 
             <div className="row g-3 gap-1 d-flex w-100 border-start p-2 border-secondary">
 
-                <h4>Ekvtime Takaishvili's N163 Public High School </h4>
+                <h4>Ekvtime Takaishvili's N163 Public School </h4>
                 <h5>High School Student</h5>
-                <h5>Focus : Actively developing full-stack skills alongside my high school studies</h5>
                 <h6>Expected Graduation: 2026</h6>
                 
             </div>
