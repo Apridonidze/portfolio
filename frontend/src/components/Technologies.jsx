2c72aee4934c8c64dfca43197dd9693fbebc5869
unknown <giorgiapridonidze08@gmail.com>
2025-11-30 03:20:18 +0400

    style : made tools components same size as technologies in Technologies.jsx

--- frontend/src/components/Technologies.jsx
+++ frontend/src/components/Technologies.jsx
@@ -60,22 +60,22 @@ const Technologies = () => {
         </div>
         </div>
 
-        <div className="other-tools">
+        <div className="other-tools pt-2">
           <div className="other-tools-header">
             <h3>Other Tools</h3>
           </div>
-          <div className="other-tools-body  d-flex flex-wrap gap-3 px-2 py-2 justify-content-start">
+          <div className="other-tools-body p-2">
             <div className="row g-2">
             {otherTools.map((tool, id) => (
               <div className="col-12 col-sm-6 col-lg-4">
               <div key={id} className="d-flex border border-secondary p-3 rounded-3  h-100 align-items-center gap-3">
-                <div className="tool-left ">
+                <div className="tool-left tech-left col-4">
                     
                   <img src={tool.img} alt={tool.name} className='bg-white rounded-circle p-1 w-100 h-100' style={{maxWidth:'200px'}}/>
                     
                 </div>
                 
-                <div className="tool-right">
+                <div className="tool-right tech-left">
                     <p className="mb-1 fs-5">{tool.name}</p>
                     <small className='fs-6'>{tool.desc}</small>
                 </div>
