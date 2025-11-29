ff5a4b529e676ad85784b279e24135f530d952ed
unknown <giorgiapridonidze08@gmail.com>
2025-11-29 14:26:09 +0400

    created Education.jsx in components

---
+++ frontend/src/components/Education.jsx
@@ -0,0 +1,18 @@
+const Education = () => {
+    return(
+        <div className="education-container">
+
+            <div className="education-header">
+                <h1>Education</h1>
+            </div>
+
+            <div className="education-container">
+                <h4>Ekvtime Takaishvili's N163 Public High School – High School Student</h4>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Education
